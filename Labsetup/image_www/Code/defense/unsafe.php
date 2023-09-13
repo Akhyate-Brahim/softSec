e0f21438a9a1833c51b3ca28bc5282b4256f498e
Akhyate-Brahim <brahim.akhyate@etu.unice.fr>
2023-09-13 15:20:39 +0200

    TD1

--- Labsetup/image_www/Code/defense/unsafe.php
+++ Labsetup/image_www/Code/defense/unsafe.php
@@ -1,10 +1,10 @@
 <?php
-// Function to create a SQL connection.
+// Function to create a sql connection.
 function getDB() {
-  $dbhost = "10.9.0.6";
-  $dbuser = "seed";
-  $dbpass = "dees";
-  $dbname = "sqllab_users";
+  $dbhost="10.9.0.6";
+  $dbuser="seed";
+  $dbpass="dees";
+  $dbname="sqllab_users";
 
   // Create a DB connection
   $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
@@ -18,7 +18,7 @@ $input_uname = $_GET['username'];
 $input_pwd = $_GET['Password'];
 $hashed_pwd = sha1($input_pwd);
 
-// Create a connection
+// create a connection
 $conn = getDB();
 
 // Prepare and execute the query
@@ -29,8 +29,16 @@ $stmt->bind_param("ss", $input_uname, $hashed_pwd);
 $stmt->execute();
 $stmt->bind_result($id, $name, $eid, $salary, $ssn);
 $stmt->fetch();
+if ($result->num_rows > 0) {
+  // only take the first row 
+  $firstrow = $result->fetch_assoc();
+  $id     = $firstrow["id"];
+  $name   = $firstrow["name"];
+  $eid    = $firstrow["eid"];
+  $salary = $firstrow["salary"];
+  $ssn    = $firstrow["ssn"];
+}
 
-// Close the SQL connection
-$stmt->close();
+// close the sql connection
 $conn->close();
 ?>
